b9f932665651c4863298ed4e9254324962049ab6
ChaoticTechSupport <ChaoticTech0111@gmail.com>
2024-10-06 20:24:03 -0400

    fixed minor inconveniences
    - removed test scene.
    - added documentation to extend script.

--- Scripts/extend.gd
+++ Scripts/extend.gd
@@ -1,5 +1,9 @@
 class_name extend
 
+# Extend class:
+# a class containing static functions for global use.
+
+#Gets all of the parents children (grand children, great grand children, ect)
 static func get_all_Children(root_node) -> Array[Node]:
 	var nodes: Array[Node] = []
 	for node in root_node.get_children():
