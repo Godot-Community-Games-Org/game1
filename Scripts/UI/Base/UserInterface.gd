f0211d25eb24d89c81b81063cf45f95aadd8175f
ChaoticTechSupport <ChaoticTech0111@gmail.com>
2024-10-08 10:08:51 -0400

    Added documentation
    Added documentation to the UserInterface and UtilityFunctions classes

--- Scripts/UI/Base/UserInterface.gd
+++ Scripts/UI/Base/UserInterface.gd
@@ -1,24 +1,38 @@
 extends Control
 class_name UserInterface
+## UserInterface Class: [br]
+## A Base UI class to contain common UI features within the game such as:[br]
+## - viewport Text Reizing (currently requires text size override to be on.).[br]
+## - Translation Switching (Unimplemented).
 
-# UtilityFunctions Class:
-# A Base UI class to contain common UI features within the game such as:
-# - viewport Text Reizing (currently requires text size override to be on.).
-# - Translation Switching (Unimplemented).
 
-#region font resizing options
-# Export Settings
-@export_group("FontResize")
-@export_enum("none", "Width", "Height", "Both") var formula_calculation: int = 0  # Setting for how to resize fonts
+@export_group("Text Resize", "resize")
 
-# viewport base size.
+## Turns Text Resizing on or off.
+@export var resize_on:bool = true
+
+enum RESIZE_MODE_ENUM 
+{
+	## the text will be resized using both the width and the height property.
+	both = 0,
+	## the text will be resized using th width property.
+	width = 2,
+	## the height will be resized using the height property.
+	height = 1
+}
+
+## Setting for how the UserInterface handles font resizing.
+@export var resize_MODE: RESIZE_MODE_ENUM = RESIZE_MODE_ENUM.both
+
+## original viewport base size.
 @onready var base_size: Vector2 = Vector2(
 	ProjectSettings.get_setting("display/window/size/viewport_width"), 
 	ProjectSettings.get_setting("display/window/size/viewport_height")
 )
-# Dictionary to hold nodes with font size overrides
+
+## Dictionary to hold nodes with font size overrides
 var font_nodes: Dictionary = {}  
-#endregion
+
 
 
 func _ready() -> void:
@@ -30,19 +44,21 @@ func _ready() -> void:
 	get_tree().get_root().size_changed.connect(font_resize)
 	font_resize()
 
-# Resizes all of the child nodes fonts.
+## Resizes all of the child nodes fonts acording to the viewport size.
 func font_resize() -> void:
+	if !resize_on:
+		return
+		
 	var viewport_size = get_viewport().size
 	for node: Node in font_nodes.keys():
 		var formula: float = 1.0
-
-		match formula_calculation:
-			1:  # Resize based on width
-				formula = viewport_size.x / base_size.x
-			2:  # Resize based on height
-				formula = viewport_size.y / base_size.y
-			3:  # Resize based on both width and height
+		match resize_MODE:
+			0:  # Resize based on both width and height
 				formula = (viewport_size.x / base_size.x + viewport_size.y / base_size.y) / 2.0
-		
+			1:  # Resize based on height
+				formula = viewport_size.y / base_size.y
+			2:  # Resize based on width
+				formula = viewport_size.x / base_size.x
+
 		# Apply the new font size to the node
 		node.add_theme_font_size_override("font_size", font_nodes[node] * formula)
--- Scripts/UtilityFunctions.gd
+++ Scripts/UtilityFunctions.gd
@@ -1,9 +1,9 @@
 class_name UtilityFunctions
 
-# UtilityFunctions class:
-# a class containing static functions for global use.
+## UtilityFunctions class:
+## a class containing static functions for global use.
 
-#Gets all of the parents children (grand children, great grand children, ect)
+## Gets all of the parents children (grand children, great grand children, ect)
 static func get_all_Children(root_node) -> Array[Node]:
 	var nodes: Array[Node] = []
 	for node in root_node.get_children():
