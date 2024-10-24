971b5ac2ad9075107ffb5506e89a15a6bf652220
ChaoticTechSupport <ChaoticTech0111@gmail.com>
2024-10-24 17:27:39 -0400

    set: base functionality

--- Scripts/Global/Global.gd
+++ Scripts/Global/Global.gd
@@ -1,3 +1,21 @@
 extends Node
+
+## Signal emitted when the player's turn ends.
 signal player_turn_end
-var pathfinder:AStarGrid2D
+
+## Reference to the AStarGrid2D node for pathfinding.
+var pathfinder: AStarGrid2D
+
+## Reference to the TileMapLayer node for interacting with the game map.
+var map: TileMapLayer
+
+## return A [PackedVector2Array] containing a path as a series of points from the start to the end position.
+## [param start]: A [Vector2] representing the starting position in the world.
+## [param end]: A [Vector2] representing the target position in the world.
+## [param tf]: A [bool] (optional, defaults to true) that toggles whether the algorithm can retun partial paths.[br]
+func calculate_path(start: Vector2, end: Vector2, tf: bool = true) -> PackedVector2Array:
+	return global.pathfinder.get_id_path(
+		map.local_to_map(start), 
+		map.local_to_map(end),
+		tf
+	)
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,65 +1,59 @@
 extends Node2D
 
-var Map: TileMapLayer
 var Actions: int = 2
-var speed: float = 100.0 # Speed of the player
+var speed: float = 200.0 # Speed of the player
 var path = [] # Array to hold the calculated path
 var path_index: int = 0 # Current index in the path
 var moving: bool = false # Movement flag
-
+var highlight_path: PackedVector2Array
 func _ready() -> void:
 	global.pathfinder = AStarGrid2D.new()
-	Map = get_parent().get_parent().get_child(0) # Adjust this as needed for clarity
-	global.pathfinder.region = Map.get_used_rect()
+	global.map = get_parent().get_parent().get_child(0) # Adjust this as needed for clarity
+	global.pathfinder.region = global.map.get_used_rect()
+	global.pathfinder.cell_size = Vector2.ONE*128
 	global.pathfinder.diagonal_mode = 1
 	global.pathfinder.update()
 	
 	# Populate solid points based on tile data
-	for I: Vector2i in Map.get_used_cells():
-		if Map.get_cell_tile_data(I).get_custom_data("Col") == 0:
+	for I: Vector2i in global.map.get_used_cells():
+		if global.map.get_cell_tile_data(I).get_custom_data("Col") == 0:
 			global.pathfinder.set_point_solid(I)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-			var cell_position = Map.local_to_map(get_global_mouse_position())
-			if cell_position in Map.get_used_cells():
-				if Map.get_cell_tile_data(cell_position).get_custom_data("Col") == 1:
-					# Start moving to the new position
-					calculate_path()
-
-func calculate_path():
-	if moving:
-		return
-	path = global.pathfinder.get_point_path(
-		Map.local_to_map(position), 
-		Map.local_to_map(get_global_mouse_position())
-	)
-	path = path.slice(1)
-	
-	# Check if a valid path was found
-	if path.size() > 0:
-		moving = true
-		path_index = 0
+			var cell_position = global.map.local_to_map(get_global_mouse_position())
+			if cell_position in global.map.get_used_cells():
+				if global.map.get_cell_tile_data(cell_position).get_custom_data("Col") == 1:
+					if highlight_path.size()-1 <= Actions:
+						path = global.calculate_path(position, get_global_mouse_position())
 
 func _process(delta: float) -> void:
-	if moving and path.size() > 0:
-		var target_position = Map.map_to_local(path[path_index])
+	if path.size() > path_index:
+		var target_position = global.map.map_to_local(path[path_index])
 		global_position = global_position.move_toward(target_position, delta * speed)
 		
 		# Check if the player has reached the target position
 		if position.distance_to(target_position) < speed * delta:
 			position = target_position
-			path_index += 1 # Move to the next point in the path
-			
-			# If reached the end of the path, stop moving
-			if path_index >= path.size():
-				moving = false
+			path_index += 1
+	else:
+		path.clear()
+		path_index = 0
+		var cell_position = global.map.local_to_map(get_global_mouse_position())
+		if cell_position in global.map.get_used_cells():
+			if global.map.get_cell_tile_data(cell_position).get_custom_data("Col") == 1:
+				highlight_path = global.calculate_path(position, get_global_mouse_position())
 	queue_redraw()
 
 func _draw() -> void:
-	if path.size() > 0:
-		for i in range(path.size() - 1):
-			var start = Map.map_to_local(path[i])-position
-			var end = Map.map_to_local(path[i + 1])-position
-			draw_line(start, end, Color(1, 0, 0), 2) # Draw the path in red with a width of 2
+	if highlight_path.size() > 0:
+		for i in highlight_path:
+			if highlight_path.find(i) <= Actions:
+				draw_circle(global.map.map_to_local(i)-position, 20, Color(1, 1, 0, 1))
+			else:
+				draw_circle(global.map.map_to_local(i)-position, 20, Color(1, 0, 0, 1))
+		if highlight_path.size()-1 <= Actions:
+			draw_rect(Rect2(global.map.map_to_local(highlight_path[highlight_path.size()-1])-(Vector2.ONE*62.5)-position, Vector2(128, 128)), Color(1, 1, 0, 1), false, 10)
+		else:
+			draw_rect(Rect2(global.map.map_to_local(highlight_path[highlight_path.size()-1])-(Vector2.ONE*62.5)-position, Vector2(128, 128)), Color(1, 0, 0, 1), false, 10)
